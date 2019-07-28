dc437d5cee8c884292bf81228cdd4ef0d5feacfc
David Milligan <david.l.milligan@gmail.com>
2019-07-28 13:29:23 -0400

    initial commit

---
+++ Generate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace rockstar
+{
+    public static class Program
+    {
+        /// <summary>
+        /// Generate the actual quine from the quine template
+        /// </summary>
+        public static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("quine.template.rock");
+            using var output = new StreamWriter(File.Open("quine.rock", FileMode.Create));
+            output.WriteLine(lines[0]);
+            foreach (var line in lines.Skip(1))
+            {
+                if (!line.StartsWith("(")) //remove comments
+                {
+                    output.Write("Rock your world with ");
+                    foreach (var ch in line.Trim())
+                    {
+                        output.Write((int)ch + ", ");
+                    }
+                    output.WriteLine("10");
+                }
+            }
+            foreach (var line in lines.Skip(1))
+            {
+                if (!line.StartsWith("("))
+                {
+                    output.WriteLine(line.Trim());
+                }
+            }
+        }
+    }
+}
